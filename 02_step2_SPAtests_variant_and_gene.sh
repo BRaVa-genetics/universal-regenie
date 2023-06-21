4e71291406e5429c4454e35391efd1c91b1c93c9
Barney Hill <barneyh@NDPH9082.local>
2023-06-21 13:53:39 +0100

    Re-add phenofile

--- 02_step2_SPAtests_variant_and_gene.sh
+++ 02_step2_SPAtests_variant_and_gene.sh
@@ -35,6 +35,11 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    --phenoFile)
+      PHENOFILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
     -s|--isSingularity)
       shift # past argument
       shift # past value

59885a8316ce9b44f9a4cfb74ec4e61d8dec6f05
Barney Hill <barneyh@NDPH9082.local>
2023-07-19 12:52:46 +0100

    Update regenie version

--- run_container.sh
+++ run_container.sh
@@ -1,9 +1,9 @@
 run_container () {
     # Load the Docker image from the tar.gz file
-    docker pull ghcr.io/rgcgithub/regenie/regenie:v3.2.7.gz
+    docker pull ghcr.io/rgcgithub/regenie/regenie:v3.2.8.gz
 
     docker run \
       -e HOME=${WD} \
       -v ${WD}/:$HOME/ \
-      ghcr.io/rgcgithub/regenie/regenie:v3.2.7.gz $cmd
+      ghcr.io/rgcgithub/regenie/regenie:v3.2.8.gz $cmd
 }
